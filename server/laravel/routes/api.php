6861ed3c03817bb0ab00d5eb863918de65ae17db
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-05 09:20:55 +0300

    Update api.php

--- server/laravel/routes/api.php
+++ server/laravel/routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
