911080fd9b07ff2f8e7abaf4eed7aa4e6900dcca
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-05 09:30:16 +0300

    Create AuthController.php

---
+++ server/laravel/app/Http/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+   public function register(UserRegisterRequest $request)
+   {
+
+   }
+    public function login(UserLoginRequest $request)
+    {
+
+    }
+    public function logout()
+    {
+
+    }
+}
