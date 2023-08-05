7d07fee491bf2de09d2bfe8d24502e0508f7c2d9
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-05 13:18:49 +0300

    loginResource added

--- server/laravel/app/Http/Controllers/AuthController.php
+++ server/laravel/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegisterRequest;
 
+use App\Http\Resources\LoginResource;
 use App\Http\Resources\UserRegisterResource;
 use App\Models\User;
 use Illuminate\Http\Response;
@@ -12,18 +13,22 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class AuthController extends Controller
 {
-   public function register(UserRegisterRequest $request)
-   {
-       $user = User::query()->create($request->validated());
-       return new UserRegisterResource($user);
-   }
-    public function login(UserLoginRequest $request)
+    public function register(UserRegisterRequest $request): UserRegisterResource
+    {
+        $user = User::query()->create($request->validated());
+
+        return new UserRegisterResource($user);
+    }
+
+    public function login(UserLoginRequest $request): LoginResource
     {
         if (!$token = auth()->attempt($request->validated())) {
             throw new HttpException(Response::HTTP_UNAUTHORIZED, 'Unauthorized');
         }
+
         return new LoginResource($token);
     }
+
     public function logout()
     {
         auth()->logout();
