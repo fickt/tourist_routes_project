4ea8b8f529f96830fe90432e768ec14c225cedfa
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-05 10:38:00 +0300

    Update AuthController.php

--- server/laravel/app/Http/Controllers/AuthController.php
+++ server/laravel/app/Http/Controllers/AuthController.php
@@ -4,20 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserLoginRequest;
 use App\Http\Requests\UserRegisterRequest;
-use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class AuthController extends Controller
 {
    public function register(UserRegisterRequest $request)
    {
-
+       $user = User::query()->create($request->validated());
+       return $user;
    }
     public function login(UserLoginRequest $request)
     {
-
+        if (!$token = auth()->attempt($request->validated())) {
+            throw new HttpException(Response::HTTP_UNAUTHORIZED, 'Unauthorized');
+        }
+        return new LoginResource($token);
     }
     public function logout()
     {
-
+        auth()->logout();
     }
 }
