0ce36db95142d03475d63181ab1c1c7dedf0f950
mmhlko <maximmhl@mail.ru>
2023-08-09 01:06:56 +0700

    добавляет спинер загрузки

--- client/src/pages/home-page/index.tsx
+++ client/src/pages/home-page/index.tsx
@@ -1,12 +1,14 @@
 import s from "./styles.module.scss"
 import { YMapComponent } from "modules/ymap";
 import { markers } from "modules/ymap/constants/markers";
+import Spiner from "ui/spiner";
 
 const HomePage = () => {   
  
     return ( 
         <div className={s.wrapper}>
-            <h2>Путешествуй и не парься</h2>            
+            <h2>Путешествуй и не парься</h2>
+            <Spiner />            
             <YMapComponent markers={markers}/>
        </div>
      );
