f4c837bcb4bc95c37e42858c0bd7962c2daef11a
mmhlko <maximmhl@mail.ru>
2023-08-10 00:13:20 +0700

    исправляет ошибки в svg спинера

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -5,7 +5,7 @@ import './styles/index.scss'
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from 'app/app-component/App';
 
-window.ResizeObserver = undefined;
+//window.ResizeObserver = undefined;
 
 const Router = process.env.REACT_APP_GH_PAGES !== 'true' ? BrowserRouter : HashRouter;
 
