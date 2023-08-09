71a1350ad1f34e06048c7e92aa6bb61f63a23da9
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-09 16:35:59 +0300

    автоматизирована установка зависимостей в app контейнере

--- server/laravel/bin/composer-installer.sh
+++ server/laravel/bin/composer-installer.sh
@@ -4,5 +4,10 @@ echo "composer-installer.sh script is running..."
 if [ -d "vendor" ]; then
     echo "Vendor directory already exists. Skipping composer install..."
 else
+    echo "Vendor directory not found.. composer install is running..."
     composer install --ignore-platform-reqs
+
+    #Чтобы избежать всяких косяков, что он не может найти папку vendor и т.д
+    echo "composer update is running..."
+    composer update --no-scripts --ignore-platform-req=ext-sockets
 fi
