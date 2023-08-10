b826148fd685fe193d5527321795ce10ef680a03
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-10 12:57:07 +0300

    Накинул миграций с маршрутами и фотографиями от маршрутов

---
+++ server/laravel/database/migrations/2023_08_10_095422_create_route_photos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('route_photos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('route_id')
+                ->constrained('routes')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->string('photo_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('route_photos');
+    }
+};
