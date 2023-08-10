b844d1fa8a69f21ad5250bc3c1a6f45a047c4fcd
Annarchive <ytpt@mail.ru>
2023-08-10 22:43:09 +0400

    Добавлены точки с запятой и отступ после const SpotPage

--- client/src/pages/spot-page/SpotPage.tsx
+++ client/src/pages/spot-page/SpotPage.tsx
@@ -1,15 +1,16 @@
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "storage/hook-types";
 import s from "./styles.module.scss"
-import { useEffect, useState } from "react";
 import { getLocalSpotsAction } from "modules/card-list/store/spots-actions";
 import { TMarker, markers } from "modules/ymap/constants/markers";
 import { YMapComponent } from "modules/ymap";
 import Card from "components/card/Card";
 
 const SpotPage = () => {
+
     const { spotId } = useParams();
-    const spots = useAppSelector(state => state?.spots?.data)
+    const spots = useAppSelector(state => state?.spots?.data);
     const [spotItem, setSpotItem] = useState<TMarker[]>(null);
 
     const dispatch = useAppDispatch();
@@ -17,15 +18,15 @@ const SpotPage = () => {
     const getSpotById = (spotId: string) => {
 
         if (spots) {
-            setSpotItem(spots?.filter(spot => spot.id === Number(spotId)))
+            setSpotItem(spots?.filter(spot => spot.id === Number(spotId)));
         }
     }
 
     useEffect(() => {
 
         if (spotId) {
-            dispatch(getLocalSpotsAction(markers))
-            getSpotById(spotId)
+            dispatch(getLocalSpotsAction(markers));
+            getSpotById(spotId);
         }
 
     }, [spotId, spots])
