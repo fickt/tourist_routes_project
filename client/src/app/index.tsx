14147ef930999c67b0a8a333eebbb11437dc30ee
Annarchive <ytpt@mail.ru>
2023-08-10 22:35:43 +0400

    Отредактировала кавычки, точки с запятой

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -1,15 +1,15 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import store from 'storage/store'
-import './styles/index.scss'
+import store from "storage/store";
+import "./styles/index.scss";
 import { BrowserRouter, HashRouter } from "react-router-dom";
-import App from 'app/app-component/App';
+import App from "app/app-component/App";
 
 //window.ResizeObserver = undefined;
 
-const Router = process.env.REACT_APP_GH_PAGES !== 'true' ? BrowserRouter : HashRouter;
+const Router = process.env.REACT_APP_GH_PAGES !== "true" ? BrowserRouter : HashRouter;
 
-const root = createRoot(document.getElementById('root'));    
+const root = createRoot(document.getElementById("root"));
 
 root.render(
     <Provider store={store}>
