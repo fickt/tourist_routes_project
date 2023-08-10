5a8de19ff3c195551832d23792a43533c8487c3d
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-10 12:02:02 +0300

    Update Route.php

--- server/laravel/app/Models/Route.php
+++ server/laravel/app/Models/Route.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Route extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'difficulty_id',
+        'longitude',
+        'latitude',
+        'distance_from_nearest_city',
+    ];
+
+    protected $guarded = [
+        'rating'
+    ];
+
+    public function photoPaths(): HasMany
+    {
+
+    }
 }
