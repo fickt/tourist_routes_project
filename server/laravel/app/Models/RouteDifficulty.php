cc54d188a9ca6aa2c0302bbf19baf80a94df42c6
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-11 08:29:45 +0300

    Сидеры для маршрутов, фоток и сложности добавлены

--- server/laravel/app/Models/RouteDifficulty.php
+++ server/laravel/app/Models/RouteDifficulty.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+/**
+ * @mixin Builder
+ *
+ * @property int $id
+ * @property string $name
+ */
 class RouteDifficulty extends Model
 {
     use HasFactory;
--- server/laravel/database/seeders/RouteSeeder.php
+++ server/laravel/database/seeders/RouteSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Route;
+use App\Models\RouteDifficulty;
+use App\Models\RoutePhoto;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,30 @@ class RouteSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $route_difficulty = RouteDifficulty::query()->where('name', '=', 'easy')->first();
+
+        $route = Route::query()->create(
+            [
+                'name' => 'Тестовый маршрут',
+                'description' => 'Дон ли, Волга ли течёт — котомку на плечо
+Боль в груди — там тайничок, открытый фомкой, не ключом
+Сколько миль ещё? Перелет короткий был не в счёт
+Долгий пыльный чёс, фургон набит коробками с мерчём
+Верим — подфартит, наши постели портативны
+Менестрелю два пути: корпоратив или квартирник
+Схемы однотипны, все теперь MC
+Ведь, смену породив, мы здесь достигли смены парадигмы
+Теперь рэп — многопартийный; бэттлов наплодив
+Я смотрю в зеркало по типу: «Сколько бед наворотил ты!»
+Я б весь рэп поработил, но всё время в пути
+У индустрии нервный тик, валокордин — стенокардийным
+Соберите суд, но победителей не судят
+Мы первые кроманьонцы — мы выбились в люди',
+                'difficulty_id' => $route_difficulty->id,
+                'longitude' => 91.4,
+                'latitude' => 53.7,
+                'distance_from_nearest_city'
+            ]
+        );
     }
 }
