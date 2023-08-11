74357a6ac731930e47ec78d2a217990ad9cff5bb
Annarchive <ytpt@mail.ru>
2023-08-11 18:50:39 +0400

    Добавила authReducer

--- client/src/storage/rootReducer.ts
+++ client/src/storage/rootReducer.ts
@@ -1,12 +1,9 @@
 import { combineReducers } from "redux";
 import { spotReducer } from "modules/card-list/store/spots-reducer";
-import { errorMessageReducer, loaderReducer, setUserReducer, userStatusReducer } from "modules/auth-form";
+import { authReducer } from "modules/auth-form";
 
 export const rootReducer = combineReducers({
     spots: spotReducer,
-    userStatus: userStatusReducer,
-    setUser: setUserReducer,
-    errorMessage: errorMessageReducer,
-    isLoaderActive: loaderReducer,
+    auth: authReducer,
 })
 
