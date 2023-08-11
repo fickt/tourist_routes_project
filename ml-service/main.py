1898e030f7d7ac6dee6d287fcbca8eb75d9f81ea
dreamLogicc <gondin02@mail.ru>
2023-08-11 16:18:24 +0700

    added api for recommender system

--- ml-service/main.py
+++ ml-service/main.py
@@ -1,8 +1,11 @@
+from fastapi import FastAPI
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.stem.porter import PorterStemmer
 import random
+
+app = FastAPI(title = 'Recommender System')
 
 data = pd.read_csv("processed.csv")
 
@@ -11,6 +14,7 @@ cv = CountVectorizer(max_features = 1500)
 vectors = cv.fit_transform(data["tags"]).toarray()
 
 ps = PorterStemmer()
+
 
 def stem(text):
     y = []
@@ -22,8 +26,10 @@ def stem(text):
 data["tags"] = data["tags"].apply(stem)
 
 
-def recommend_on_survey(list_of_answers: list, vectors) -> list:
-    likes = list(filter(lambda x: x != "", list_of_answers))
+@app.post('/recommend-on-servey')
+def recommend_on_survey(likes: str):
+    arr = likes.split(',')
+    likes = list(filter(lambda x: x != "", arr))
     to_recommend = []
     try:
         for caption in likes:
@@ -39,31 +45,20 @@ def recommend_on_survey(list_of_answers: list, vectors) -> list:
 
         to_recommend = random.sample(sorted(set(to_recommend)), 5)
 
-        return to_recommend
+        result = {}
+        for i in range(len(to_recommend)):
+            result[f'place{i}'] = {
+                'index': to_recommend[i],
+                'name': data.iloc[to_recommend[i]]['name'],
+                'type': data.iloc[to_recommend[i]]['type']
+            }
+
+        return result
 
     except Exception as ex:
         print(ex)
 
 
-def recommend_on_image(caption: str, vectors) -> list:
-    to_recommend = []
-    try:
-        vector = cv.transform([caption]).toarray()
-        distances = []
-        for e in vectors:
-            distances.append(cosine_similarity(vector, [list(e)]))
-
-        distances = sorted(
-            list(enumerate(distances)), reverse = True, key = lambda x: x[1]
-        )[0:5]
-        to_recommend.extend([place[0] for place in distances])
-
-        return to_recommend
-
-    except Exception as ex:
-        print(ex)
-
-
-if __name__ == '__main__':
-    print(data['caption'][0])
-    print(data['tags'][0])
+@app.post('/recommend-on-image')
+def recommend_on_image(caption: str):
+    pass
