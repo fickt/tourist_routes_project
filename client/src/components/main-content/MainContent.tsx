c9bbbdde348863731523d3c62e888fd67a719a0c
mmhlko <maximmhl@mail.ru>
2023-08-12 14:34:00 +0700

    добавлен background на главную секцию

--- client/src/components/main-content/MainContent.tsx
+++ client/src/components/main-content/MainContent.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useState } from "react";
 import s from "./styles.module.scss";
 import { SearchForm } from "ui/search/SearchForm";
 import { CardList } from "modules/card-list";
+import backImage from "./assets/map_bg.jpg";
 
 export const MainContent = () => {
 
@@ -18,7 +19,7 @@ export const MainContent = () => {
 
     return (
         <>
-            <section className={s.map_section}>
+            <section className={s.map_section} style={{backgroundImage: `url(${backImage})`}}>
                 <div className={s.main__header}>
                     <h1 className={s.header__title}>Отправьтесь<br />в чудесное путешествие</h1>
                     <p className={s.header__text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
