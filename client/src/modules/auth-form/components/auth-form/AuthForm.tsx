b3a909547a915f512aeaaa3e17f9d865ca79868c
Annarchive <ytpt@mail.ru>
2023-08-15 01:51:37 +0400

    Добавила комментарии для AuthForm

--- client/src/modules/auth-form/components/auth-form/AuthForm.tsx
+++ client/src/modules/auth-form/components/auth-form/AuthForm.tsx
@@ -9,12 +9,13 @@ import { emailRules, nicknameRules } from "modules/auth-form/constants/formRules
 import { PasswordInput } from "modules/auth-form/components/form-input/PasswordInput";
 import { FormButton } from "modules/auth-form/components/form-button/FormButton";
 import { handleErrorMessage, handleLoaderActive, handleSetUser, handleUserAuth, handleUserReg } from "modules/auth-form/store/authActions";
-import { useAppSelector, useAppDispatch } from "storage/hook-types";
+import { useAppDispatch, useAppSelector } from "storage/hook-types";
 import { TFormData } from "modules/auth-form/store/types/authTypes";
 import { authError, authLoader, isUserReg } from "modules/auth-form/store/authSelectors";
 import { ErrorMessage } from "ui/error-message/ErrorMessage";
 import { TAuthFormProps } from "./types";
 import { FormLink } from "modules/auth-form/components/form-link/FormLink";
+import { AppRoutes, RoutePath } from "pages/routeConfig";
 
 export const AuthForm = ({ type }: TAuthFormProps) => {
 
@@ -28,26 +29,31 @@ export const AuthForm = ({ type }: TAuthFormProps) => {
     }, [])
 
     const [form] = Form.useForm<FormInstance>();
+    // собрана информация из инпутов, которую ввёл user
     const [formData, setFormData] = useState<TFormData | null>(null);
 
-    const setUserAuth = (isRegistration: boolean) => {
-        dispatch(isRegistration ? handleUserReg(true) : handleUserAuth(true))
+    // отслеживание полей формы
+    const handleFormChange = (changedFields: any) => {
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            ...changedFields,
+        }));
+    };
+
+    // отправка запроса (login или reg)
+    const sendForm = async (values: any) => {
+        userReg
+            ? await performAuth(values.email, values.password, false)
+            : await performAuth(values.email, values.password, true)
     }
 
-    const setSuccessMessage = (isRegistration: boolean, response: any) => {
-        dispatch(
-            isRegistration
-                ? handleErrorMessage(`Пользователь ${response.data.user.email} зарегистрирован`)
-                : handleErrorMessage(`Вы вошли как ${response.data.user.email}`)
-        )
-    }
-
+    // структура запроса (login или reg)
     const performAuth = async (email: string, password: string, isRegistration: boolean) => {
         dispatch(handleLoaderActive(true));
         try {
             const response = isRegistration
-                ? await AuthService.registration(email, password)
-                : await AuthService.login(email, password);
+                ? await AuthService.registration(email, password) //reg
+                : await AuthService.login(email, password); // login
             Cookies.set("token", response.data.accessToken);
             Cookies.set("login", response.data.user.email);
             setUserAuth(isRegistration);
@@ -60,18 +66,18 @@ export const AuthForm = ({ type }: TAuthFormProps) => {
         }
     }
 
-    const handleFormChange = (changedFields: any) => {
-        setFormData((prevFormData) => ({
-            ...prevFormData,
-            ...changedFields,
-        }));
-    };
+    // сообщение об успешной авторизации (login или reg)
+    const setSuccessMessage = (isRegistration: boolean, response: any) => {
+        dispatch(
+            isRegistration
+                ? handleErrorMessage(`Пользователь ${response.data.user.email} зарегистрирован`)
+                : handleErrorMessage(`Вы вошли как ${response.data.user.email}`)
+        )
+    }
 
-    const formValidation = async (values: any) => {
-        console.log(formData);
-        userReg
-            ? await performAuth(values.email, values.password, false)
-            : await performAuth(values.email, values.password, true)
+    // пользователь авторизован (login или reg)
+    const setUserAuth = (isRegistration: boolean) => {
+        dispatch(isRegistration ? handleUserReg(true) : handleUserAuth(true))
     }
 
     return (
@@ -81,7 +87,7 @@ export const AuthForm = ({ type }: TAuthFormProps) => {
             className={s.form}
             initialValues={{remember: true}}
             autoComplete="off"
-            onFinish={formValidation}
+            onFinish={sendForm}
             onValuesChange={handleFormChange}
         >
             <div className={s.form__inputs}>
@@ -89,7 +95,7 @@ export const AuthForm = ({ type }: TAuthFormProps) => {
                 <FormInput name="email" rules={emailRules} placeholder="E-mail" />
                 <PasswordInput type={type} />
                 <FormButton
-                    value={type === "login" ? "Войти" : "Зарегистрироваться"}
+                    value={type === RoutePath[AppRoutes.LOGIN] ? "Войти" : "Зарегистрироваться"}
                     onClick={form.submit}
                     disabled={!form.isFieldsTouched(true) || form.getFieldsError()
                         .filter(({errors}) => errors.length).length > 0
