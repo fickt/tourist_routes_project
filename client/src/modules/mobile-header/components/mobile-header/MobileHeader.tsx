6d0401be4b264b13858389223e90f0430315634e
mmhlko <maximmhl@mail.ru>
2023-08-15 01:39:02 +0700

    добавляет иконки в модуль меню

--- client/src/modules/mobile-header/components/mobile-header/MobileHeader.tsx
+++ client/src/modules/mobile-header/components/mobile-header/MobileHeader.tsx
@@ -1,4 +1,11 @@
-import s from "./styles.module.scss"
+import s from "./styles.module.scss";
+import HomeIcon from "./assets/home.svg";
+import SearchIcon from "./assets/search.svg";
+import FavoritesIcon from "./assets/favorites.svg";
+import ProfileIcon from "./assets/profile.svg";
+import { Link } from "react-router-dom";
+import { RoutePath } from "pages/routeConfig";
+
 
 export const MobileHeader = () => {
 
@@ -6,10 +13,10 @@ export const MobileHeader = () => {
         <div className={s.container}>
             <nav className={s.mobile_menu}>
                 <div className={s.menu__wrapper}>
-                    <a href="#">Главная</a>
-                    <a href="#">Поиск</a>
-                    <a href="#">Избранное</a>
-                    <a href="#">Профиль</a>
+                    <Link to={RoutePath.home}><HomeIcon /></Link>
+                    <Link to={RoutePath.home}><SearchIcon /></Link>
+                    <Link to={RoutePath.home}><FavoritesIcon /></Link>
+                    <Link to={RoutePath.home}><ProfileIcon /></Link>
                 </div>
             </nav>
         </div>
