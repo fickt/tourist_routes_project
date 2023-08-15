0de086591d90d98e9f1a678d856c63a8056cfb3a
Annarchive <ytpt@mail.ru>
2023-08-15 17:54:59 +0400

    Убрала credentials: true

--- client/src/modules/auth-form/api/index.ts
+++ client/src/modules/auth-form/api/index.ts
@@ -5,10 +5,7 @@ import { TAuthResponse } from "modules/auth-form/store/types/authTypes";
 import { handleErrorMessage } from "modules/auth-form/store/authActions";
 
 export const API_URL = `http://localhost:8080/api/auth`;
-export const $api = axios.create({
-    withCredentials: true,
-    baseURL: API_URL,
-})
+export const $api = axios.create({baseURL: API_URL})
 
 $api.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${Cookies.get("token")}`;
