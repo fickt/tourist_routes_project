024ed87af5ce64e7d38f3d681cb14f9d149a1acb
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-15 09:22:03 +0300

    Добавил метод index в RouteController

--- server/laravel/app/Http/Controllers/RouteController.php
+++ server/laravel/app/Http/Controllers/RouteController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Resources\RouteResource;
 use App\Models\Route;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class RouteController extends Controller
 {
@@ -15,5 +17,20 @@ class RouteController extends Controller
                 ->with(['difficulty', 'photoPaths', 'categories'])
                 ->get()
         );
+    }
+
+    public function show(int $routeId)
+    {
+        $route = Route::query()
+            ->find($routeId)
+            ->with(['difficulty', 'photoPaths', 'categories'])
+            ->first();
+
+        return $route !== null
+            ? RouteResource::make($route)
+            : throw new HttpException(
+                Response::HTTP_NOT_FOUND,
+                'Route with id:' . $routeId . ' has not been found!'
+            );
     }
 }
