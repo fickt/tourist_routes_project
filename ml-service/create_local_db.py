8c4d4afd7b41ba14b115a35fc7aa1bdde95ed191
dreamLogicc <gondin02@mail.ru>
2023-08-15 15:03:43 +0700

    Update dokerfile

--- ml-service/create_local_db.py
+++ ml-service/create_local_db.py
@@ -7,8 +7,5 @@ alchemy_engine = create_engine('postgresql+psycopg2://root:root@127.0.0.1:5432/p
 connection = alchemy_engine.connect()
 
 data = pd.read_csv('data_for_db.csv')
-connection.execute(text(
-    "INSERT INTO route_difficulties (name, created_at,updated_at) VALUES ('Новичок', '2023-08-11', '2023-08-11' ), ('Знающий', '2023-08-11','2023-08-11' ), ('Опытный', '2023-08-11', '2023-08-11' );"))
-connection.commit()
 
 data.to_sql('routes', connection, if_exists = 'append', index = False)
