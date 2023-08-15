d0b2e271241df1e35a29d66c4f0bce1de49adda5
mmhlko <maximmhl@mail.ru>
2023-08-15 14:11:51 +0700

    Добавляет активную кнопку в headerMobile, устраняет лишний ререндер

--- client/src/modules/mobile-header/components/mobile-header/MobileHeader.tsx
+++ client/src/modules/mobile-header/components/mobile-header/MobileHeader.tsx
@@ -1,24 +1,45 @@
 import s from "./styles.module.scss";
-import HomeIcon from "./assets/home.svg";
-import SearchIcon from "./assets/search.svg";
-import FavoritesIcon from "./assets/favorites.svg";
-import ProfileIcon from "./assets/profile.svg";
-import { Link } from "react-router-dom";
-import { RoutePath } from "pages/routeConfig";
+import { Link, useLocation } from "react-router-dom";
+import { SyntheticEvent } from "react";
+import { menuLinks } from "./constants/menuLinks";
+import classNames from "classnames";
 
 
 export const MobileHeader = () => {
+
+    const location = useLocation();
+    const { pathname } = location;
+
+    const handleClick = (e: SyntheticEvent<HTMLAnchorElement>, path: string) => {
+
+        if (location.pathname === path) {
+            e.preventDefault();
+        }
+    };
 
     return (
         <div className={s.container}>
             <nav className={s.mobile_menu}>
                 <div className={s.menu__wrapper}>
-                    <Link to={RoutePath.home}><HomeIcon /></Link>
-                    <Link to={RoutePath.home}><SearchIcon /></Link>
-                    <Link to={RoutePath.home}><FavoritesIcon /></Link>
-                    <Link to={RoutePath.home}><ProfileIcon /></Link>
+                    {menuLinks.map((link, index) => (
+                        <Link key={index} to={link.path} onClick={(e) => handleClick(e, link.path)}>
+                            <span className={classNames({ [s.menu__icon_filled]: pathname === link.path })}>
+                                {link.icon}
+                            </span>
+                        </Link>
+                    ))}
                 </div>
             </nav>
         </div>
     )
 }
+
+/*                     <Link to={RoutePath.home} onClick={(e) => handleClick(e, RoutePath.home)}>
+                        <span className={classNames({ [s.menu__icon_filled]: pathname === RoutePath.home })}>
+                            <HomeIcon />
+                        </span>
+                    </Link>
+                    <Link to={RoutePath.home} ><span className={classNames({ [s.menu__icon_filled]: pathname === RoutePath.spots })}><SearchIcon /></span></Link>
+                    <Link to={RoutePath.home} ><span className={classNames({ [s.menu__icon_filled]: false })}><FavoritesIcon /></span></Link>
+                    <Link to={RoutePath.home} ><span className={classNames({ [s.menu__icon_filled]: false })}><ProfileIcon /></span></Link>
+                 */
