92f2232fdce94e500d65f8cafe75efe1df04c49f
Annarchive <ytpt@mail.ru>
2023-08-16 20:25:02 +0400

    Добавила параметры в TFormButtonProps

--- client/src/modules/auth-form/components/form-button/types.ts
+++ client/src/modules/auth-form/components/form-button/types.ts
@@ -1,7 +1,9 @@
 import { MouseEvent } from "react";
+import { FormInstance } from "antd";
 
 export type TFormButtonProps = {
     value: string;
     onClick: (e: MouseEvent<HTMLButtonElement>) => void;
-    disabled: boolean;
+    form: FormInstance;
+    loader: string;
 }
