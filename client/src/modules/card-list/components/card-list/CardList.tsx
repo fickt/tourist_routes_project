72a667fc8570e03e593fdb9e8e20ab5bd4bf20ba
mmhlko <maximmhl@mail.ru>
2023-08-17 01:06:26 +0700

    добавляет компонент сортировки в список мест

--- client/src/modules/card-list/components/card-list/CardList.tsx
+++ client/src/modules/card-list/components/card-list/CardList.tsx
@@ -1,17 +1,21 @@
 import s from "./styles.module.scss";
 import { memo } from "react";
-import Card from "components/card/Card";
 import { useAppSelector } from "storage/hookTypes";
 import { spotsSelectors } from "modules/card-list/store/spotsSelectors";
 import InstaCard from "components/instaCard/InstaCard";
+import { Sorting } from "modules/card-list/components/Sorting/Sorting";
+import { sortOptions } from "modules/card-list/constants/sortOptions";
 
 export const CardList = memo(() => {
 
     const spots = useAppSelector(spotsSelectors);
 
     return (
-        <div className={s.cards}>
-            {spots?.map((spot) => <InstaCard key={spot.id} {...spot} />)}
-        </div>
+        <>
+            <Sorting options={sortOptions} />
+            <div className={s.cards}>
+                {spots?.map((spot) => <InstaCard key={spot.id} {...spot} />)}
+            </div>
+        </>
     );
 });
