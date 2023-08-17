1078f1801ce0c5c6675e38eaf3358293afec6989
Annarchive <ytpt@mail.ru>
2023-08-17 17:58:09 +0400

    Исправила RoutePath у linkPath

--- client/src/pages/auth-page/AuthPage.tsx
+++ client/src/pages/auth-page/AuthPage.tsx
@@ -1,16 +1,14 @@
 import s from "./styles.module.scss";
 import { AuthForm } from "modules/auth-form/components/auth-form/AuthForm";
 import { FormHeader } from "components/form-header/FormHeader";
-import { AppRoutes, RoutePath } from "pages/routeConfig";
+import { RoutePath } from "pages/routeConfig";
 import { TAuthPageProps } from "pages/types";
 
 const AuthPage = ({ isRegister }: TAuthPageProps) => {
 
     const title = isRegister ? "Регистрация" : "Добро пожаловать";
-    const text = isRegister
-        ? "Уже зарегистрированы? Войдите в аккаунт."
-        : "Чтобы продолжить, пожалуйста, войдите в аккаунт";
-    const linkPath = isRegister ? RoutePath[AppRoutes.AUTH_LOGIN] : RoutePath[AppRoutes.AUTH_REGISTER];
+    const text = isRegister ? "Уже зарегистрированы? Войдите в аккаунт." : "Чтобы продолжить, пожалуйста, войдите в аккаунт";
+    const linkPath = isRegister ? RoutePath.login : RoutePath.register;
     const headerLink = isRegister ? "Уже зарегистрированы?" : "Ещё не зарегистрированы?";
     const textLink = isRegister ? "Войдите в аккаунт." : "Зарегистрируйтесь здесь.";
 
