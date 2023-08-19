d9efe80c1f2bbfdfdf687f3bc385b100fecf81c7
Annarchive <ytpt@mail.ru>
2023-08-19 16:35:35 +0400

    Унификация api со spots api

--- client/src/modules/auth-form/api/authService.ts
+++ client/src/modules/auth-form/api/authService.ts
@@ -1,18 +1,18 @@
 import { AxiosResponse } from "axios";
 import { RoutePath } from "pages/routeConfig";
 import { TAuthResponse } from "modules/auth-form/store/types/authTypes";
-import $api from "modules/auth-form/api/index";
+import { api } from "utils/api";
 
 export class authService {
     static async login(email: string, password: string): Promise<AxiosResponse<TAuthResponse>> {
-        return $api.post(RoutePath.login, { email, password });
+        return api.post(`/auth${RoutePath.login}`, { email, password });
     }
 
     static async register(nickname: string, email: string, password: string): Promise<AxiosResponse<TAuthResponse>> {
-        return $api.post(RoutePath.register, { nickname, email, password });
+        return api.post(`/auth${RoutePath.register}`, { nickname, email, password });
     }
 
     static async logout(): Promise<void> {
-        return $api.post<typeof RoutePath.logout, null, void>(RoutePath.logout);
+        return api.post<typeof RoutePath.logout, null, void>(`/auth${RoutePath.logout}`);
     }
 }
