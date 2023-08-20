422cc26b63b1d4483592a2b63a47ee33470f87bb
mmhlko <maximmhl@mail.ru>
2023-08-20 15:53:45 +0700

    добавляет обработку ошибки при проблеме с сервером

--- client/src/modules/card-list/api/SpotsServise.ts
+++ client/src/modules/card-list/api/SpotsServise.ts
@@ -2,10 +2,11 @@ import { AxiosResponse } from "axios";
 import { RoutePath } from "pages/routeConfig";
 import { TSpotsState } from "modules/card-list/store/types/spotsReducerTypes";
 import { api } from "utils/api";
+import { TSpotRoutes } from "modules/card-list//types/spotRoutes";
 
 export class SpotsService {
     
-    fetchSpots(): Promise<AxiosResponse<TSpotsState>> {
+    fetchSpots(): Promise<AxiosResponse<TSpotRoutes>> {
         return api.get<TSpotsState>(RoutePath.spots);
     }
 }
--- client/src/modules/card-list/components/spot-route-list/SpotRouteList.tsx
+++ client/src/modules/card-list/components/spot-route-list/SpotRouteList.tsx
@@ -1,22 +1,34 @@
 import s from "./styles.module.scss";
-import React, { memo, useEffect } from "react";
+import React, { memo, useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "storage/hookTypes";
 import { spotRoutesSelector } from "modules/card-list/store/spotsSelectors";
 import { apiSpots } from "modules/card-list/api/SpotsServise";
 import { getSpotRoutes } from "modules/card-list/store/spotsActions";
 import { SpotRoute } from "components/spot-route/SpotRoute";
 import { TSpotRoute } from "modules/card-list/types/spotRoutes";
+import { ErrorMessage } from "ui/error-message/ErrorMessage";
+import { PreloaderCar } from "ui/preloader/PreloaderCar";
 
 export const SpotRouteList = memo(() => {
 
     const dispatch = useAppDispatch();
     const spotRoutes = useAppSelector(spotRoutesSelector);
-
+    const [error, setError] = useState<string>();
+    const [loading, setLoading] = useState(false);   
+    
     useEffect(() => {
+        //пока не оформлял код ниже, закончил на стадии обработки ошибки
         if (!spotRoutes) {
+            setLoading(true)
             apiSpots.fetchSpots()
                 .then(data => {
                     dispatch(getSpotRoutes(data.data))
+                })
+                .catch(() => {
+                    setError("Ошибка на стороне сервера")
+                })
+                .finally(() => {
+                    setLoading(false)
                 })
         }
     }, [])
@@ -27,7 +39,8 @@ export const SpotRouteList = memo(() => {
             <div className={s.cards}>
                 {spotRoutes?.map((spot: TSpotRoute) => <SpotRoute key={spot.id} {...spot} />)}
             </div>
-
+            {loading && <PreloaderCar />}
+            {!loading && (error && <ErrorMessage errorText={error} />)}
         </>
     );
 });
