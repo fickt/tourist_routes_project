b4b7147c89f234f899c08e73efca8cda0bdc2dfe
Annarchive <ytpt@mail.ru>
2023-08-20 17:17:47 +0400

    Изменила названия экшенов для ошибки и лоадера

--- client/src/modules/auth-form/store/authActionsTypeNames.ts
+++ client/src/modules/auth-form/store/authActionsTypeNames.ts
@@ -1,5 +1,5 @@
 export const IS_USER_REG = "IS_USER_REG";
 export const IS_USER_AUTH = "IS_USER_AUTH";
 export const SET_USER = "SET_USER";
-export const ERROR_MESSAGE = "ERROR_MESSAGE";
-export const IS_LOADER_ACTIVE = "IS_LOADER_ACTIVE";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const AUTH_LOADER_ACTIVE = "AUTH_LOADER_ACTIVE";
