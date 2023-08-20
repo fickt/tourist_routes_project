c979471d73e88b2884f00bd94c216d80c3de5f83
Annarchive <ytpt@mail.ru>
2023-08-20 17:19:10 +0400

    Вынесла в отдельный файл константы и сделала деструктуризацию

--- client/src/pages/auth-page/AuthPage.tsx
+++ client/src/pages/auth-page/AuthPage.tsx
@@ -1,16 +1,12 @@
 import s from "./styles.module.scss";
 import { AuthForm } from "modules/auth-form/components/auth-form/AuthForm";
 import { FormHeader } from "components/form-header/FormHeader";
-import { RoutePath } from "pages/routeConfig";
 import { TAuthPageProps } from "pages/types";
+import { getAuthPageTextContent } from "pages/auth-page/constants/authPageTextContent";
 
 export const AuthPage = ({ isRegister }: TAuthPageProps) => {
 
-    const title = isRegister ? "Регистрация" : "Добро пожаловать";
-    const text = isRegister ? "" : "Чтобы продолжить, пожалуйста, войдите в аккаунт";
-    const linkPath = isRegister ? RoutePath.auth_login : RoutePath.auth_register;
-    const headerLink = isRegister ? "Уже зарегистрированы?" : "Ещё не зарегистрированы?";
-    const textLink = isRegister ? "Войдите в аккаунт." : "Зарегистрируйтесь здесь.";
+    const { title, text, linkPath, headerLink, textLink } = getAuthPageTextContent(isRegister);
 
     return (
         <div className={s.auth}>
