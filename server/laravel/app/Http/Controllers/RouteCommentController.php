f2b3626b4b55e98134548ce62c90f480f0ced04c
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-21 16:30:22 +0300

    Создал контроллер для комментов от маршрута

---
+++ server/laravel/app/Http/Controllers/RouteCommentController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Route;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class RouteCommentController extends Controller
+{
+    public function store(int $routeId)
+    {
+        try {
+            $route = Route::query()->findOrFail($routeId);
+        } catch (Exception) {
+            throw new HttpException(
+                Response::HTTP_NOT_FOUND,
+                'Route with id: ' . $routeId . 'has not been found!'
+            );
+        }
+        
+    }
+}
