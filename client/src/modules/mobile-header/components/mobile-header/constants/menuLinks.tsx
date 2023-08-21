60fa34b1351350eaf0fc2500ed01cb95a9f6f899
Annarchive <ytpt@mail.ru>
2023-08-22 01:24:33 +0400

    Неавторизованный user не должен попасть на страницу profile, его перекинет на страницу login

--- client/src/modules/mobile-header/components/mobile-header/constants/menuLinks.tsx
+++ client/src/modules/mobile-header/components/mobile-header/constants/menuLinks.tsx
@@ -3,19 +3,10 @@ import HomeIcon from "modules/mobile-header/components/mobile-header/assets/home
 import SearchIcon from "modules/mobile-header/components/mobile-header/assets/search.svg";
 import FavoritesIcon from "modules/mobile-header/components/mobile-header/assets/favorites.svg";
 import ProfileIcon from "modules/mobile-header/components/mobile-header/assets/profile.svg";
-import Cookies from "js-cookie";
-
-const checkUserAuth = () => {
-    if (Cookies.get("token")) {
-        return RoutePath.profile;
-    } else {
-        return RoutePath.auth_login;
-    }
-}
 
 export const menuLinks = [
     {path: RoutePath.home, icon: <HomeIcon />},
     {path: RoutePath.spots, icon: <SearchIcon />},
     {path: RoutePath.favorites, icon: <FavoritesIcon />},
-    {path: checkUserAuth(), icon: <ProfileIcon />},
+    {path: RoutePath.profile, icon: <ProfileIcon />},
 ]
