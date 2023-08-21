a3f28b234c50ff07f84ebf5982b32994acdb3089
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-21 17:43:05 +0300

    Реализован метод store в RouteCommentController

--- server/laravel/app/Http/Controllers/RouteCommentController.php
+++ server/laravel/app/Http/Controllers/RouteCommentController.php
@@ -2,24 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RouteCommentRequest;
 use App\Models\Route;
-use Exception;
-use Illuminate\Http\Request;
+use App\Models\RouteComment;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class RouteCommentController extends Controller
 {
     public function store(int $routeId, RouteCommentRequest $request)
     {
-        try {
-            $route = Route::query()->findOrFail($routeId);
-        } catch (Exception) {
-            throw new HttpException(
+        return Route::query()->find($routeId)->exists()
+            ? RouteComment::query()->create([
+                $request->validated(),
+                ['route_id' => $routeId],
+                ['user_id' => Auth::id()]
+            ])
+            : throw new HttpException(
                 Response::HTTP_NOT_FOUND,
                 'Route with id: ' . $routeId . 'has not been found!'
             );
-        }
-
     }
 }
