53ae4fdf53547c2cd17e3cd00f4879aad35c7e0f
Annarchive <ytpt@mail.ru>
2023-08-22 21:52:40 +0400

    Убрала проверку есть ли токен, добавила стилей

--- client/src/pages/profile-page/ProfilePage.tsx
+++ client/src/pages/profile-page/ProfilePage.tsx
@@ -1,44 +1,23 @@
-import React, { useEffect } from "react";
 import ArrowRightIcon from "./images/arrow-right.svg";
 import s from "./styles.module.scss";
-import { useAppDispatch, useAppSelector } from "storage/hookTypes";
+import { useAppSelector } from "storage/hookTypes";
 import { authUser } from "modules/auth-form/store/authSelectors";
 import classNames from "classnames";
-import Cookies from "js-cookie";
-import { TUser } from "modules/auth-form/store/types/authTypes";
-import { handleSetUser } from "modules/auth-form/store/authActions";
 import { RoutePath } from "pages/routeConfig";
 
 export const ProfilePage = () => {
 
-    const dispatch = useAppDispatch();
     const user = useAppSelector(authUser);
-
-    useEffect(() => {
-        if (Cookies.get("token")) {
-            const userData: TUser = {
-                nickname: Cookies.get("nickname"),
-                email: Cookies.get("email"),
-            }
-            dispatch(handleSetUser(userData));
-        }
-    }, [])
 
     return (
         <div className={s.wrapper}>
             <div className={s.header}>
                 <div className={s.header__info}>
                     <div className={s.header__info_wrapper}>
-                        {user
-                            ? <span className={s.header__info_text}>{user.nickname[0]}</span>
-                            : <span className={s.header__info_text}>Н</span>
-                        }
+                        {user && <span className={s.header__info_text}>{user.nickname[0]}</span>}
                     </div>
                     <div className={s.settings}>
-                        {user
-                            ? <h2 className={s.title}>{user.nickname}</h2>
-                            : <h2 className={s.title}>Никнейм</h2>
-                        }
+                        {user && <h2 className={s.title}>{user.nickname}</h2>}
                         <p>Управление аккаунтом</p>
                     </div>
                 </div>
