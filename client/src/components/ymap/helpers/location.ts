e07a1e3e226d80f0688d1106cd8ab16877c3f79b
mmhlko <maximmhl@mail.ru>
2023-08-22 19:19:40 +0700

    рефакторинг компонента карты, обработка ошибки отсутсвия геоданных

---
+++ client/src/components/ymap/helpers/location.ts
@@ -0,0 +1,17 @@
+export type TLocationSuccess = (pos: GeolocationPosition) => void;
+export type TLocationFail = (err: GeolocationPositionError) => void;
+
+export const getLocation = (success: TLocationSuccess, onFail?:TLocationFail) => {
+
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    };
+
+    function error(err: GeolocationPositionError) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);        
+    }
+
+    navigator.geolocation.getCurrentPosition(success, onFail || error, options);
+}
