f4cd9dc0230a9aee796dc04d077bef363f2892c4
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-24 19:33:43 +0300

    Добавил ограничение на количество маршрутов из csv файлов

--- server/laravel/database/seeders/RouteSeederFromCsv.php
+++ server/laravel/database/seeders/RouteSeederFromCsv.php
@@ -9,32 +9,39 @@ use Illuminate\Database\Seeder;
 
 class RouteSeederFromCsv extends Seeder
 {
+
+    private const NUMBER_OF_ROUTES = 30;
+
     /**
      * Run the database seeds.
      */
     public static function run(): void
     {
-        if (($open = fopen(storage_path() . "/routes" . "/data_for_db.csv", "r")) !== FALSE) {
-            fgetcsv($open, 1000, ",");
-            while (($data = fgetcsv($open, 1000, ",")) !== FALSE) {
-                $route = Route::query()->create(
-                    [
-                        'name' => $data[0],
-                        'description' => $data[1],
-                        'latitude' => $data[2],
-                        'longitude' => $data[3],
-                        'difficulty_id' => $data[4],
-                        'distance_from_nearest_city' => $data[5],
-                        'rating' => 0
-                    ]
-                );
-                RoutePhoto::query()->create(
-                    [
-                        'route_id' => $route->id,
-                        'photo_path' => 'image_route_zaglushka.jpg'
-                    ]);
-            }
-            fclose($open);
+        $open = fopen(storage_path() . "/routes" . "/data_for_db.csv", "r");
+        fgetcsv($open, 1000, ",");
+
+        $count = 0;
+        while (($data = fgetcsv($open, 1000, ",")) && !($count === self::NUMBER_OF_ROUTES)) {
+            $route = Route::query()->create(
+                [
+                    'name' => $data[0],
+                    'description' => $data[1],
+                    'latitude' => $data[2],
+                    'longitude' => $data[3],
+                    'difficulty_id' => $data[4],
+                    'distance_from_nearest_city' => $data[5],
+                    'rating' => 0
+                ]
+            );
+            RoutePhoto::query()->create(
+                [
+                    'route_id' => $route->id,
+                    'photo_path' => 'image_route_zaglushka.jpg'
+                ]);
+
+            $count++;
         }
+        fclose($open);
+
     }
 }
