ecacd40183d245045c85b1b4529790e52d6c3fb5
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-24 17:46:09 +0300

    Create RouteSeederFromCsv.php

---
+++ server/laravel/database/seeders/RouteSeederFromCsv.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RouteSeederFromCsv extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public static function run(): void
+    {
+        //
+    }
+}
