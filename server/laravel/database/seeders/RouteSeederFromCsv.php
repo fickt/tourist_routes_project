f77aa297de3c99431262755fed018f779e5f4345
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-25 15:01:24 +0300

    Вынес в константу путь до csv с маршрутами

--- server/laravel/database/seeders/RouteSeederFromCsv.php
+++ server/laravel/database/seeders/RouteSeederFromCsv.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Seeder;
 
 class RouteSeederFromCsv extends Seeder
 {
-
+    private const PATH_ROUTES_FOR_DB = "/routes/data_for_db.csv";
     private const NUMBER_OF_ROUTES = 30;
 
     /**
@@ -17,7 +17,7 @@ class RouteSeederFromCsv extends Seeder
      */
     public static function run(): void
     {
-        $open = fopen(storage_path() . "/routes" . "/data_for_db.csv", "r");
+        $open = fopen(storage_path() . self::PATH_ROUTES_FOR_DB, "r");
         fgetcsv($open, 1000, ",");
 
         $count = 0;
@@ -42,6 +42,5 @@ class RouteSeederFromCsv extends Seeder
             $count++;
         }
         fclose($open);
-
     }
 }
