f3d5c5fa82db6df5bbc58fb3a16ce1aeb668e3d7
Annarchive <ytpt@mail.ru>
2023-08-27 15:24:52 +0400

    Выделила компонент ProfileSection

--- client/src/modules/profile/components/profile/Profile.tsx
+++ client/src/modules/profile/components/profile/Profile.tsx
@@ -1,11 +1,11 @@
 import s from "./styles.module.scss";
-import { Link } from "react-router-dom";
 import { RoutePath } from "pages/routeConfig";
 import { Button } from "ui/button/Button";
 import { useAppSelector} from "storage/hookTypes";
 import { isRecommended } from "modules/questions/store/questionsSelectors";
 import { QuestionsPopup } from "modules/questions/components/questions-popup/QuestionsPopup";
 import { ProfileHeader } from "modules/profile/components/profile-header/ProfileHeader";
+import { ProfileSection } from "modules/profile/components/profile-section/ProfileSection";
 
 export const Profile = () => {
 
@@ -15,12 +15,8 @@ export const Profile = () => {
         <div className={s.profile}>
             <ProfileHeader text="Управление аккаунтом" path={RoutePath.settings} />
             <div className={s.main}>
-                <Link className={s.main__fav} to={RoutePath.mySpots}>
-                    <span className={s.main__fav_text}>Мои места</span>
-                </Link>
-                <Link className={s.main__fav} to={RoutePath.favorites}>
-                    <span className={s.main__fav_text}>Избранное</span>
-                </Link>
+                <ProfileSection path={RoutePath.mySpots} title="Мои места" />
+                <ProfileSection path={RoutePath.favorites} title="Избранное" />
             </div>
             <h2 className={s.title}>Рекомендации</h2>
             {recommended ? "Сюда будут подгружены рекомендации пользователя с сервера" : <QuestionsPopup />}
