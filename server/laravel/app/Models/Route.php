e85a1aac08537868be85cdcc716014c73e8788d9
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-28 16:12:37 +0300

    Рефактор, перекинул бизнес логику из RouteController в RouteModel

--- server/laravel/app/Models/Route.php
+++ server/laravel/app/Models/Route.php
@@ -3,11 +3,16 @@
 namespace App\Models;
 
 use App\Casts\FloatRound;
+use Exception;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
  * App\Models\Route
@@ -22,10 +27,10 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
  * @property float $rating
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\RouteCategory> $categories
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\RouteComment> $comments
+ * @property-read Collection<int, \App\Models\RouteCategory> $categories
+ * @property-read Collection<int, \App\Models\RouteComment> $comments
  * @property-read \App\Models\RouteDifficulty $difficulty
- * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\RoutePhoto> $photoPaths
+ * @property-read Collection<int, \App\Models\RoutePhoto> $photoPaths
  * @mixin \Eloquent
  */
 class Route extends Model
@@ -49,6 +54,58 @@ class Route extends Model
     protected $casts = [
         'rating' => FloatRound::class
     ];
+
+    /**
+     * Получить все Route
+     *
+     * Доступные queries:
+     * ?difficulty= (отфильтровать по уровням сложности)
+     * ?category= (отфильтровать по категориям)
+     * ?search= (поиск по названию и описанию Route)
+     *
+     * @return Collection
+     */
+    public function get_routes(): Collection
+    {
+        $query = self::query()->with(['difficulty', 'photoPaths', 'categories', 'comments.user']);
+
+        if (Request::query('difficulty')) {
+            $difficulty = explode(',', Request::query('difficulty'));
+            $query->whereHas('difficulty', function ($q) use ($difficulty) {
+                $q->whereIn('name', $difficulty);
+            });
+        }
+
+        if (Request::query('category')) {
+            $category = explode(',', Request::query('category'));
+            $query->whereHas('categories', function ($q) use ($category) {
+                $q->whereIn('name', $category);
+            });
+        }
+
+        if ($search = Request::query('search')) {
+            $query
+                ->where('name', 'LIKE', "%$search%")
+                ->orWhere('description', 'LIKE', "%$search%");
+        }
+        return $query->get();
+    }
+
+    public function get_route_by_id(int $routeId): Route
+    {
+        try {
+            $route = self::query()
+                ->findOrFail($routeId)
+                ->with(['difficulty', 'photoPaths', 'categories', 'comments.user'])
+                ->first();
+        } catch (Exception) {
+            throw new HttpException(
+                Response::HTTP_NOT_FOUND,
+                'Route with id:' . $routeId . ' has not been found!'
+            );
+        }
+        return $route;
+    }
 
     /**
      * Возвращает пути до фоток от Route
