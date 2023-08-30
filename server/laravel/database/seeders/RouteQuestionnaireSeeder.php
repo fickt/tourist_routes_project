4221d10b42b4c40740cea73ae9f927d7bb9afd55
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-30 18:22:37 +0300

    Добавил ещё 7 категорий к анкете

--- server/laravel/database/seeders/RouteQuestionnaireSeeder.php
+++ server/laravel/database/seeders/RouteQuestionnaireSeeder.php
@@ -22,9 +22,52 @@ class RouteQuestionnaireSeeder extends Seeder
                 'name' => 'Test questionnaire'
             ]
         );
-        $mountainsPhoto = RouteQuestionnairePhoto::query()->create([
+
+        RouteQuestionnairePhoto::query()->create([
             'photo' => 'questionnaire_gori_photo.jpg',
             'category_id' => RouteCategory::query()->where('name', '=', 'горы')->first()->id,
+            'questionnaire_id' => $questionnaire->id
+        ]);
+
+        RouteQuestionnairePhoto::query()->create([
+            'photo' => 'questionnaire_ozero_photo.jpg',
+            'category_id' => RouteCategory::query()->where('name', '=', 'озёра')->first()->id,
+            'questionnaire_id' => $questionnaire->id
+        ]);
+
+        RouteQuestionnairePhoto::query()->create([
+            'photo' => 'questionnaire_istoricheskie_mesta_photo.jpg',
+            'category_id' => RouteCategory::query()->where('name', '=', 'исторические места')->first()->id,
+            'questionnaire_id' => $questionnaire->id
+        ]);
+
+        RouteQuestionnairePhoto::query()->create([
+            'photo' => 'questionnaire_zona_otdiha_photo.jpg',
+            'category_id' => RouteCategory::query()->where('name', '=', 'зоны отдыха')->first()->id,
+            'questionnaire_id' => $questionnaire->id
+        ]);
+
+        RouteQuestionnairePhoto::query()->create([
+            'photo' => 'questionnaire_reki_photo.jpg',
+            'category_id' => RouteCategory::query()->where('name', '=', 'реки')->first()->id,
+            'questionnaire_id' => $questionnaire->id
+        ]);
+
+        RouteQuestionnairePhoto::query()->create([
+            'photo' => 'questionnaire_pesheri_photo.jpg',
+            'category_id' => RouteCategory::query()->where('name', '=', 'пещеры')->first()->id,
+            'questionnaire_id' => $questionnaire->id
+        ]);
+
+        RouteQuestionnairePhoto::query()->create([
+            'photo' => 'questionnaire_gornolizhni_komplex_photo.jpg',
+            'category_id' => RouteCategory::query()->where('name', '=', 'горнолыжные комплексы')->first()->id,
+            'questionnaire_id' => $questionnaire->id
+        ]);
+
+        RouteQuestionnairePhoto::query()->create([
+            'photo' => 'questionnaire_kurgan_photo.jpg',
+            'category_id' => RouteCategory::query()->where('name', '=', 'курганы')->first()->id,
             'questionnaire_id' => $questionnaire->id
         ]);
     }
