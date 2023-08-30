4c14603c315a42f22134bcdceb68f780b469ca8d
Annarchive <ytpt@mail.ru>
2023-08-30 18:21:31 +0400

    Вынесла в ui-компонент FavoriteMarker

---
+++ client/src/ui/favorite-marker/FavoriteMarker.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import s from "./styles.module.scss";
+import FavoritesIcon from "./assets/favorites.svg";
+
+export const FavoriteMarker = () => {
+
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleClick = () => {
+        setIsActive(!isActive);
+    }
+
+    return (
+        <button
+            className={`${s.favorite} ${isActive && s.favorite_active}`}
+            onClick={toggleClick}>
+            <FavoritesIcon className={s.favorite_icon} />
+        </button>
+    )
+}
