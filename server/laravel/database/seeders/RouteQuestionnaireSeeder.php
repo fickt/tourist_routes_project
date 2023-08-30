78404089d653fd781f0acdd5d287435099ee3b4a
Michael Arbuzov <garrys2machinima@gmail.com>
2023-08-30 16:03:58 +0300

    Написан простой сидер для анкеты

---
+++ server/laravel/database/seeders/RouteQuestionnaireSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\RouteCategory;
+use App\Models\RouteQuestionnaire;
+use App\Models\RouteQuestionnairePhoto;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RouteQuestionnaireSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        /**
+         * @var RouteQuestionnaire $questionnaire
+         */
+        $questionnaire = RouteQuestionnaire::query()->create([
+                'name' => 'Test questionnaire'
+            ]
+        );
+        $mountainsPhoto = RouteQuestionnairePhoto::query()->create([
+            'photo' => 'questionnaire_gori_photo',
+            'category_id' => RouteCategory::query()->where('name', '=', 'горы')->first()->id
+
+        ]);
+        $questionnaire->photos()->save($mountainsPhoto);
+
+
+    }
+}
