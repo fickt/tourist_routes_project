7ba2ded883abfc681da776b43368374b0e850552
Michael Arbuzov <garrys2machinima@gmail.com>
2023-09-02 15:29:47 +0300

    Добавил rabbitmq в конфиги

--- server/laravel/config/queue.php
+++ server/laravel/config/queue.php
@@ -70,7 +70,22 @@ return [
             'block_for' => null,
             'after_commit' => false,
         ],
+        'rabbitmq' => [
 
+            'driver' => 'rabbitmq',
+            'hosts' => [
+                [
+                    'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER', 'guest'),
+                    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+                    'vhost' => env('RABBITMQ_VHOST', '/'),
+                ],
+                // ...
+            ],
+
+            // ...
+        ],
     ],
 
     /*
