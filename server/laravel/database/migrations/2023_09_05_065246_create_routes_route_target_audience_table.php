13830fb84b7f41c78490502376dd69f7f9ed9feb
Michael Arbuzov <garrys2machinima@gmail.com>
2023-09-05 13:57:01 +0700

    Накинул миграцию для many-to-many маршрутом и целевой аудитории

---
+++ server/laravel/database/migrations/2023_09_05_065246_create_routes_route_target_audience_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('routes_route_target_audience', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('route_id')
+                ->constrained('routes')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->foreignId('target_audience_id')
+                ->constrained('target_audiences')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('routes_route_target_audience');
+    }
+};
