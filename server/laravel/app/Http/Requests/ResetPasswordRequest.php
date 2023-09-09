e355dc80ed3359aa35218345246de303b80d1653
Michael Arbuzov <garrys2machinima@gmail.com>
2023-09-09 15:50:30 +0700

    Create ResetPasswordRequest.php

---
+++ server/laravel/app/Http/Requests/ResetPasswordRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class ResetPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|string|email|exists:users|min:1|max:255',
+            'verification_code' => 'required|integer|between:1000,9999'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.exists' => throw new HttpException(Response::HTTP_NOT_FOUND, 'Аккаунт с такой почтой не существует!')
+        ];
+    }
+}
