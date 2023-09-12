85a25a1d2277a1d2357aabc23a7e0f7737d0049c
Michael Arbuzov <garrys2machinima@gmail.com>
2023-09-12 16:19:23 +0700

    Создал клиент для общения с ml service

--- server/laravel/app/Clients/MlServiceClient.php
+++ server/laravel/app/Clients/MlServiceClient.php
@@ -2,10 +2,16 @@
 
 namespace App\Clients;
 
+use Illuminate\Support\Facades\Http;
+
 class MlServiceClient
 {
+    private const URL_ML_SERVICE = 'http://ml-service:9000/recommend-on-image';
     public function findRouteByImage($image)
     {
-        
+        $response = Http::post(self::URL_ML_SERVICE, [
+            'file' => $image
+        ]);
+        print_r($response->status());
     }
 }
--- server/laravel/app/Http/Controllers/RouteFindByImageController.php
+++ server/laravel/app/Http/Controllers/RouteFindByImageController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Clients\MlServiceClient;
 use Illuminate\Http\Request;
 
 class RouteFindByImageController extends Controller
 {
+    public function __construct(protected MlServiceClient $client)
+    {
+    }
+
     public function show(Request $request)
     {
-        //$request
+        return $this->client->findRouteByImage($request->file('image'));
     }
 }
