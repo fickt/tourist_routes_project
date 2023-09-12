75e35ed734b985a5668cafae6ab82d9441766454
Michael Arbuzov <garrys2machinima@gmail.com>
2023-09-12 19:58:45 +0700

    Update MlServiceClient.php

--- server/laravel/app/Clients/MlServiceClient.php
+++ server/laravel/app/Clients/MlServiceClient.php
@@ -9,9 +9,11 @@ class MlServiceClient
     private const URL_ML_SERVICE = 'http://ml-service:9000/recommend-on-image';
     public function findRouteByImage($image)
     {
+        $encoded = base64_encode($image);
         $response = Http::post(self::URL_ML_SERVICE, [
-            'file' => $image
+            'file' => base64_encode($image)
         ]);
-        print_r($response->status());
+        print_r($response->json());
+        return base64_decode($encoded);
     }
 }
