76a0668964992e936f7ca8c6b7d92352b4212d8a
Michael Arbuzov <garrys2machinima@gmail.com>
2023-09-12 21:13:37 +0700

    Update MlServiceClient.php

--- server/laravel/app/Clients/MlServiceClient.php
+++ server/laravel/app/Clients/MlServiceClient.php
@@ -7,11 +7,12 @@ use Illuminate\Support\Facades\Http;
 class MlServiceClient
 {
     private const URL_ML_SERVICE = 'http://ml-service:9000/recommend-on-image';
+
     public function findRouteByImage($image)
     {
         $encoded = base64_encode(file_get_contents($image));
         $response = Http::post(self::URL_ML_SERVICE, [
-            base64_encode($image)
+           'file' => base64_encode($image)
         ]);
       //  print_r($response->json());
         return $response->json();
