c025237ad9eb5aac358868f74e012640f53899ec
Xverlxrd <72937924+Xverlxrd@users.noreply.github.com>
2023-09-12 17:57:56 +0700

    Исправил названия

--- client/src/components/logo/Logo.tsx
+++ client/src/components/logo/Logo.tsx
@@ -3,11 +3,12 @@ import {Link, useLocation} from "react-router-dom";
 import s from "./styles.module.scss";
 import {Typography} from "antd";
 import BackIcon from "./assets/backIcon.svg";
+import {useAppSelector} from "storage/hookTypes";
 
 const {Title} = Typography;
 
 export const Logo = () => {
-
+    const {spotId} = useAppSelector(state => state.spotId)
     const location = useLocation();
     const goBack = (e: SyntheticEvent<HTMLAnchorElement>) => {
         e.stopPropagation();
@@ -17,7 +18,7 @@ export const Logo = () => {
     const handleClick = (e: SyntheticEvent<HTMLAnchorElement>) => {
         location.pathname === "/" && e.preventDefault();
     };
-
+    console.log(location.pathname)
     const shouldRenderBackIcon = () => {
         return location.pathname !== "/" &&
             location.pathname !== "/profile" &&
@@ -25,10 +26,35 @@ export const Logo = () => {
             location.pathname !== "/questions";
     };
 
+    const nameOfPage = () => {
+        switch (location.pathname) {
+        case "/":
+            return "Наши маршруты";
+        case "/auth/login":
+            return "Войти";
+        case "/auth/register":
+            return "Регистрация";
+        case "/profile":
+            return "Личный кабинет";
+        case "/favorites":
+            return "Избранное";
+        case "/location":
+            return "Ближайшие места";
+        case "/settings":
+            return "Управление аккаунтом";
+        case "/settings_info":
+            return "Управление аккаунтом";
+        case `/spotMap/:${spotId}`:
+            return "Маршрут";
+        default:
+            return "О месте";
+        }
+    }
+
     return (
         <>
             <Link to={"/"} className={s.logo} onClick={handleClick}>
-                <Title level={2} className={s.logo__text}>logo</Title>
+                <Title level={2} className={s.logo__text}>{nameOfPage()}</Title>
             </Link>
 
             {shouldRenderBackIcon() && (
