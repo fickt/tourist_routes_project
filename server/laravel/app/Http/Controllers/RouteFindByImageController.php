c0cd15a373b342a506505b913fd72749090ffda6
Michael Arbuzov <garrys2machinima@gmail.com>
2023-09-12 15:47:56 +0700

    Создал роут и контроллер для поиска маршрутов по картинке

---
+++ server/laravel/app/Http/Controllers/RouteFindByImageController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RouteFindByImageController extends Controller
+{
+    public function show(Request $request)
+    {
+        //$request
+    }
+}
