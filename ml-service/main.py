2f60273da24850bd841396235f206cfe872dbf77
dreamLogicc <gondin02@mail.ru>
2023-09-12 20:27:37 +0700

    Update main.py

--- ml-service/main.py
+++ ml-service/main.py
@@ -32,13 +32,10 @@ place_recommender = PlaceRecommender()
 landscape_clf = keras.models.load_model('clfv2.keras')
 
 
-def read_image(file):
-    image = Image.open(io.BytesIO(file)).resize((150, 150))
-    return image
 
 @app.post('/recommend-on-image')
-async def recommend_on_image(file: str):
-    image = Image.open(io.BytesIO(base64.decodebytes(bytes(file, "utf-8"))))
+async def recommend_on_image(file):
+    image = Image.open(io.BytesIO(base64.decodebytes(bytes(file['file'], "utf-8"))))
 
     if np.array(image).shape[0] > 1080 or np.array(image).shape[0] > 1920 or np.array(image).shape[2] != 3:
         raise HTTPException(status_code = 400, detail = 'Invalid file shape')
