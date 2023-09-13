f42fe0c2f65d8b223ddb3508243d2286a8d1c1cf
Michael Arbuzov <garrys2machinima@gmail.com>
2023-09-13 14:02:02 +0700

    Update composer-installer.sh

--- server/laravel/bin/composer-installer.sh
+++ server/laravel/bin/composer-installer.sh
@@ -9,7 +9,7 @@ else
     echo "Vendor directory not found.. composer install is running..."
     composer install --ignore-platform-reqs
 
-    #Чтобы избежать всяких косяков, что он не может найти папку vendor и т.д
     echo "composer update is running..."
-    composer update --no-scripts --ignore-platform-req=ext-sockets
+    php artisan cache:clear
+    php artisan optimize
 fi
