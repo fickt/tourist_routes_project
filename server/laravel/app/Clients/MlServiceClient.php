ea8e047d7a27004a48391412bcf58cd2fe5fa8bf
Michael Arbuzov <garrys2machinima@gmail.com>
2023-09-13 12:00:54 +0700

    Фича с поиском по картинке почти реализована

--- server/laravel/app/Clients/MlServiceClient.php
+++ server/laravel/app/Clients/MlServiceClient.php
@@ -2,18 +2,18 @@
 
 namespace App\Clients;
 
+use App\Models\Route;
 use Illuminate\Support\Facades\Http;
 
 class MlServiceClient
 {
     private const URL_ML_SERVICE = 'http://ml-service:9000/recommend-on-image';
+
     public function findRouteByImage($image)
     {
-        $encoded = base64_encode(file_get_contents($image));
         $response = Http::post(self::URL_ML_SERVICE, [
            'file' => base64_encode(file_get_contents($image))
-        ]);
-      //  print_r($response->json());
-        return $response->json();
+        ])->json();
+        return Route::query()->whereIn('id', $response)->get();
     }
 }
--- server/laravel/app/Http/Controllers/RouteFindByImageController.php
+++ server/laravel/app/Http/Controllers/RouteFindByImageController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Clients\MlServiceClient;
+use App\Http\Resources\RouteResource;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class RouteFindByImageController extends Controller
 {
@@ -11,8 +13,8 @@ class RouteFindByImageController extends Controller
     {
     }
 
-    public function show(Request $request)
+    public function show(Request $request): AnonymousResourceCollection
     {
-        return $this->client->findRouteByImage($request->file('image'));
+        return RouteResource::collection($this->client->findRouteByImage($request->file('image')));
     }
 }
